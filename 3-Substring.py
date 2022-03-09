608589095e0d45faea23bc6badd737df529c8f56
frason5566 <fanif81@gmail.com>
2022-03-09 20:12:53 +0800

    0309

---
+++ 3-Substring.py
@@ -0,0 +1,33 @@
+def O(s):
+    """
+    :type s: str
+    :rtype: int
+    """
+    Lc=[]
+    maxL = 0
+    for i in range(len(s)): 
+        if s[i] not in Lc:
+            Lc.append(s[i])
+            print("Lc Append", s[i])
+            # maxL = len(Lc)
+        else:    
+            maxL = len(Lc) if len(Lc)>maxL else maxL 
+            Lc = Lc[i+1:]
+            print("Lc = ", Lc)
+        maxL = len(Lc) if len(Lc)>maxL else maxL    
+    return maxL
+            
+def main():
+    Str1 = "abcabccba"
+    Str2 = "bbbbb"
+    Str3 = "abcccccdba"
+    Str4 = "pwwkew"
+    Str5 = "aab"
+    print(O(Str1))
+    print(O(Str2))
+    print(O(Str3))
+    print(O(Str4))
+    print(O(Str5))
+
+
+main()
