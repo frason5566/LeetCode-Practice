a79ad0eb53b3c7d7c88c5056e3e318e1f5b82fe1
frason5566 <fanif81@gmail.com>
2022-03-17 17:47:15 +0800

    0317NB

--- Blind75/153-Find.py
+++ Blind75/153-Find.py
@@ -10,26 +10,27 @@ def FF(nums):
     if len(nums) == 2: return nums[0] if nums[0] < nums[1] else nums[1]
     l=0
     r=len(nums)-1
+
     while l < r:
         m = (l+r) // 2
-        print(m)
-        if nums[r] < nums[m] :
-            l = m + 1
+        print("l =",l,",r =",r,",m =",m)
+        if nums[m] > nums[r] or (r == l+1 and nums[l]>nums[r]):
+            l = m + 1 
         else:
-            r = m 
-    return nums[m]
+            r = m
+    
+    return nums[l] if nums[l]<nums[r] else nums[r]
 
 def main():
-    N=[6,7,0,1,2]
+    N=[0,1,2,3,4]
     print(FF(N))
-    N=[11,13,15,17]
+    N=[4,0,1,2,3]
     print(FF(N))
-    N=[3,4,5,1,2]
+    N=[3,4,0,1,2]
     print(FF(N))
-    0,1,2,3,4
-    4,0,1,2,3
-    3,4,0,1,2
-    2,3,4,0,1
-    1,2,3,4,0
+    N=[2,3,4,0,1]
+    print(FF(N))
+    N=[1,2,3,4,0]
+    print(FF(N))
 
 main()
--- Blind75/33-Search.py
+++ Blind75/33-Search.py
@@ -1,17 +1,66 @@
 def S(nums,target):
+    if target not in nums: return -1
+    if len(nums) == 1: return 0 
     res = -1
-    
+    l = 0
+    r = len(nums)-1
+    m = 0
+    while m != res:
+        m = (l+r) //2
+        if nums[m] == target:
+            res = m 
+        elif target in nums[l:m+1]:
+            r = m 
+            res = m if len(nums[l:m+1]) == 1 else -1
+        else:
+            l = m + 1  
+            res = m if len(nums[m:r+1]) == 1 else -1
+    return res
 
+def SS(nums,target):
+    if len(nums)==1: return -1 if nums[0] != target else 0
+    res = -1
+    l = 0
+    r = len(nums)-1
+    m = (l+r)//2
+    while l<r:
+        m = (l+r)//2
+        print("m=",m,",nums[m]=",nums[m])
+        if nums[m] == target or nums[r]==target:
+            res = m if nums[m] == target else r
+            l=r
+        elif nums[m] < target and target < nums[r]:
+            l = m + 1
+        elif nums[m] < target:
+            r = m
+        elif nums[m] > target and target < nums[l]:
+            l = m+1
+        else:    
+            r = m
+        
     return res
 
 
 def main():
-    N=[4,5,6,7,0,1,2]
-    print(S(N),0)
-    N=[4,5,6,7,0,1,2]
-    print(S(N),3)
-    N=[1]
-    print(S(N),0)
-
+    # N=[4,5,6,7,0,1,2]
+    # print(SS(N,0))
+    # N=[4,5,6,7,0,1,2]
+    # print(SS(N,3))
+    N=[1,3]
+    print(SS(N,3))
+    N=[1,3]
+    print(SS(N,1))
+    # N=[1]
+    # print(SS(N,0))
+    # N=[0,1,2,3,4]
+    # print(S(N,2))
+    # N=[4,0,1,2,3]
+    # print(S(N,2))
+    # N=[3,4,0,1,2]
+    # print(S(N,2))
+    # N=[2,3,4,0,1]
+    # print(S(N,0))
+    # N=[1,2,3,4,0]
+    # print(S(N,2))
 
 main()
