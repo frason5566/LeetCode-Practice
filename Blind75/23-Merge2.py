4b8a531a5beca9023a18e979a3914f5efd8efc55
frason5566 <fanif81@gmail.com>
2022-04-16 12:54:38 +0800

    0416

--- Blind75/23-Merge2.py
+++ Blind75/23-Merge2.py
@@ -3,12 +3,20 @@ class ListNode:
         self.val = val
         self.next = next
 
-def M(lists): 
-    ll=len(lists)
-    C=[ListNode()]*ll
-    for i in range (ll-1):
-        C[i]=lists[i]
+def M(lists):
     res=ListNode()
+    ll=len(lists)
+    if ll == 0: return res
+    elif ll == 1: return lists[0]
+    
+    
+    C=[ListNode()]*ll
+    ch = 0
+    for i in range (ll):
+        if lists[i]!=None:
+            C[i]=lists[i]
+            ch += 1
+    if ch == 0 :return res
     T=res
     Flag = True
     while Flag:
@@ -21,18 +29,16 @@ def M(lists):
             if C[i] == None:
                 continue
             elif C[i].val<=minN:
-                print("i =", i , "Val =",C[i].val)
+                minN = C[i].val
                 ptr.val=C[i].val
                 minI=i
         T.next = ptr
-        print(type(C[minI]),"  ",minI)
         C[minI] = C[minI].next
-        print("Next, C[minI].next = ",C[minI])
         T = T.next
         for i in range(len(C)):
             if C[i]!=None:
                 Flag = True
-    return res.next
+    return res
 
 def N_ins(H,v):
     ptr = ListNode()
@@ -60,29 +66,28 @@ def N_Dis(H):
 
 def main():
     L1=ListNode()
-    N_ins(L1,1)
-    N_ins(L1,2)
-    N_ins(L1,4)
-    print("L1 = ",end='')
-    N_Dis(L1)
+    # N_ins(L1,1)
+    # N_ins(L1,2)
+    # N_ins(L1,8)
+    # print("L1 = ",end='')
+    # N_Dis(L1)
     L1=L1.next
     L2=ListNode()
-    N_ins(L2,5)
-    N_ins(L2,8)
-    N_ins(L2,9)
-    print("L2 = ",end='')
-    N_Dis(L2)
+    # N_ins(L2,4)
+    # N_ins(L2,5)
+    # N_ins(L2,9)
+    # print("L2 = ",end='')
+    # N_Dis(L2)
     L2=L2.next
     L3=ListNode()
-    N_ins(L3,3)
-    N_ins(L3,6)
-    N_ins(L3,7)
-    print("L3 = ",end='')
-    N_Dis(L3)
+    # N_ins(L3,3)
+    # N_ins(L3,6)
+    # N_ins(L3,7)
+    # print("L3 = ",end='')
+    # N_Dis(L3)
     L3=L3.next
-    A=[L1,L2,L3]
+    A=[L1,L3,L2]
     N_Dis(M(A))
-    A=[[],[],[]]
-    print(A)
+
 
 main()
