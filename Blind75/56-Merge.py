eee8a9b507329b3af5deb03136d41b70333e67fc
frason5566 <fanif81@gmail.com>
2022-05-01 00:56:09 +0800

    0430

--- Blind75/56-Merge.py
+++ Blind75/56-Merge.py
@@ -1,5 +1,5 @@
 def M (intervals):
-    if len(intervals)==1:return intervals
+    if len(intervals) == 1: return intervals
     intervals.sort(key=lambda x:x[0])
     res = [intervals[0]]
     
@@ -15,42 +15,22 @@ def M (intervals):
     
     return res
 
-class ITV:
-    def __init__(self):
-        self.val = 0
-        self.isEnd = False
-
 def MM(intervals):
-    if len(intervals)==1:return intervals
-    res = [ITV] * 10001
+    if len(intervals) == 1: return intervals
     ans = []
-    
+    start = []
+    end = []
     for item in intervals:
-        for i in range(item[0],item[1]+1):
-            temp = ITV()
-            res[i] = temp
-            temp.val += 1
-            if i == item[1]:
-                res[i].isEnd = True
+        start.append(item[0])
+        end.append(item[1])
+    s = e = 0
     flag = 0
-    for i in range(10001):
-        T = res[i]
-        if T.val == 0 and flag == 0:
-            continue
-        elif T.val >= 1 and flag == 0:
-            temp = [0] * 2
-            temp[0] = i
-            flag = 1
-        elif T.val >= 1 and flag == 1:
-            if T.isEnd == True:
-                temp[1] = i-1
-                flag = 0
-                ans.append(temp)
-        elif T.val == 0 and flag == 1:
-            temp[1] = i-1
-            flag = 0
-            ans.append(temp)
-    
+    while e == len(end):
+        while True:
+            st = start[s]
+            s += 1
+
+
     return ans
 
 
