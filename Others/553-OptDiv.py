765b58761f9fc38e7cb5e8576f18a43610aaf149
frason5566 <fanif81@gmail.com>
2022-06-01 15:08:48 +0800

    0601

--- Others/553-OptDiv.py
+++ Others/553-OptDiv.py
@@ -1,8 +1,12 @@
 def O (nums):
     res = ""
     if len(nums) == 1: res += str(nums[0])
-    if len(nums) == 2: res =  str(nums[0]) + "/" + str(nums[1])
- 
+    elif len(nums) == 2: res =  str(nums[0]) + "/" + str(nums[1])
+    else:
+        res += str(nums[0])+ "/(" 
+        for i in range(1,len(nums)-1):
+            res += str(nums[i]) + "/"
+        res += str(nums[len(nums)-1]) + ")"
     return res
 
 def main():
