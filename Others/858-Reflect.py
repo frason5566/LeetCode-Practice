2b6b1e271b44368b23f5a114741064b2e26d1cb0
frason5566 <fanif81@gmail.com>
2022-08-04 11:26:19 +0800

    0804

---
+++ Others/858-Reflect.py
@@ -0,0 +1,8 @@
+def mirrorReflection(self, p: int, q: int) -> int:
+    while p % 2 == 0 and q % 2 == 0: 
+        p, q = p / 2, q / 2
+    return int(1 - p % 2 + q % 2)
+
+def main():
+    print(mirrorReflection(2,1))
+main()
